dde5389d0517ff3a7dd7ccd52177386a077101c5
yueban <fbzhh007@gmail.com>
2016-04-05 23:11:59 +0800

    create project

---
+++ app/src/main/java/bigfat/com/rxbusdemo/ui/Activity2.java
@@ -0,0 +1,51 @@
+package bigfat.com.rxbusdemo.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import bigfat.com.rxbusdemo.R;
+import bigfat.com.rxbusdemo.common.BaseActivity;
+import bigfat.com.rxbusdemo.event.CommonEvent;
+import bigfat.com.rxbusdemo.rxbus.RxBus;
+import rx.functions.Action1;
+
+public class Activity2 extends BaseActivity {
+    private static final String TAG = Activity2.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        //RxBus.postEventSticky(new CommonEvent(TAG + "_sticky"));
+        RxBus.postEvent(new CommonEvent(TAG + "_normal"));
+
+        //noinspection ConstantConditions
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Activity2.this, Activity3.class));
+            }
+        });
+
+        mBusSticky
+                .ofType(CommonEvent.class)
+                .subscribe(new Action1<CommonEvent>() {
+                    @Override
+                    public void call(CommonEvent commonEvent) {
+                        Log.i(TAG, commonEvent.message);
+                    }
+                });
+
+        mBus
+                .ofType(CommonEvent.class)
+                .subscribe(new Action1<CommonEvent>() {
+                    @Override
+                    public void call(CommonEvent commonEvent) {
+                        Log.i(TAG, commonEvent.message);
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/bigfat/com/rxbusdemo/ui/Activity3.java
@@ -0,0 +1,54 @@
+package bigfat.com.rxbusdemo.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import bigfat.com.rxbusdemo.R;
+import bigfat.com.rxbusdemo.common.BaseActivity;
+import bigfat.com.rxbusdemo.event.CommonEvent;
+import bigfat.com.rxbusdemo.rxbus.RxBus;
+import rx.functions.Action1;
+
+public class Activity3 extends BaseActivity {
+    private static final String TAG = Activity3.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+
+        RxBus.postEventSticky(new CommonEvent(TAG + "_sticky"));
+        RxBus.postEvent(new CommonEvent(TAG + "_normal"));
+
+        //noinspection ConstantConditions
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Activity3.this, MainActivity.class).addFlags(
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP));
+            }
+        });
+
+        mBusSticky
+            .ofType(CommonEvent.class)
+            .compose(this.<CommonEvent>bindToLifecycle())
+            .subscribe(new Action1<CommonEvent>() {
+                @Override
+                public void call(CommonEvent commonEvent) {
+                    Log.i(TAG, commonEvent.message);
+                }
+            });
+
+        mBus
+            .ofType(CommonEvent.class)
+            .compose(this.<CommonEvent>bindToLifecycle())
+            .subscribe(new Action1<CommonEvent>() {
+                @Override
+                public void call(CommonEvent commonEvent) {
+                    Log.i(TAG, commonEvent.message);
+                }
+            });
+    }
+}
